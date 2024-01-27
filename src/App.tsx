69003ebfb9c6585c3635326a9cfa784f868e1e34
weeee9 <teletubby332@gmail.com>
2024-01-27 15:25:03 +0800

    feat: refactor category rendering in `src/App.tsx`
    - Modify the import statement for `./categories.styles.scss` in `src/App.tsx`
    - Change the `title` and `imageUrl` values for the `categories` array in `src/App.tsx`
    - Update the JSX code in the `return` statement of the `App` component in `src/App.tsx`
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,47 @@
-import './categories.styles.scss'
+import './categories.styles.scss';
+
+const categories = [
+  {
+    id: 1,
+    title: 'hats',
+    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+  },
+  {
+    id: 2,
+    title: 'jackets',
+    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+  },
+  {
+    id: 3,
+    title: 'sneakers',
+    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+  },
+  {
+    id: 4,
+    title: 'womens',
+    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+  },
+  {
+    id: 5,
+    title: 'mens',
+    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+  },
+];
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-      subtitle: 'Shop Now',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-      subtitle: 'Shop Now',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-      subtitle: 'Shop Now',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-      subtitle: 'Shop Now',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-      subtitle: 'Shop Now',
-    },
-  ];
-
   return (
     <div className='categories-container'>
       {categories.map((category) => {
-        const { id, title, subtitle } = category;
+        const { id, title, imageUrl } = category;
         return (
           <div key={id} className='category-container'>
-            <div className='background-image' />
+            <div
+              className='background-image'
+              style={{ backgroundImage: `url(${imageUrl})` }}
+            />
             <div className='category-body-container'>
               <h2>{title}</h2>
-              <p>{subtitle}</p>
+              <p>Shop Now</p>
             </div>
           </div>
         );
