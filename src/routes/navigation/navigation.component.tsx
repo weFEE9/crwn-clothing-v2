df6964d5905c663ae0ef39cf18ee22b906befb18
weeee9 <teletubby332@gmail.com>
2024-01-27 19:48:51 +0800

    feat: add logo and nav links to header
    - Add a Link component import from 'react-router-dom'
    - Add a logo container with the text "Logo" inside a Link component
    - Add a nav links container with a Link component inside, containing the text "SHOP"
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,12 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
     <>
-      <div>
-        <h1>I am the navigation bar</h1>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <div>Logo</div>
+        </Link>
+
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
     </>
