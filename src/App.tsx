8c2e8a6fe145b734ec27ede18a95a4fcc979de34
weeee9 <teletubby332@gmail.com>
2024-01-27 19:27:47 +0800

    feat: refactor file structure and import statements
    - Remove unnecessary imports and code related to category list in `App.tsx`
    - Add a new file `home.component.tsx` with a `Home` component
    - Move the category list code from `App.tsx` to `home.component.tsx`
    - Import `BrowserRouter` in `index.tsx`
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,7 @@
-import { Category } from './components/category-item/category-item.component';
-import CategoryList from './components/category-list/category-list.component';
-
-const categories: Category[] = [
-  {
-    id: 1,
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-  },
-  {
-    id: 2,
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-  },
-  {
-    id: 3,
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-  },
-  {
-    id: 4,
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-  },
-  {
-    id: 5,
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-  },
-];
+import Home from './components/routes/home/home.component';
 
 const App = () => {
-  return <CategoryList categories={categories} />;
+  return <Home />;
 };
 
 export default App;
---
+++ src/components/routes/home/home.component.tsx
@@ -0,0 +1,36 @@
+import { Category } from '../../category-item/category-item.component';
+import CategoryList from '../../category-list/category-list.component';
+
+const Home = () => {
+  const categories: Category[] = [
+    {
+      id: 1,
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    },
+    {
+      id: 2,
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    },
+    {
+      id: 3,
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    },
+    {
+      id: 4,
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    },
+    {
+      id: 5,
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ];
+
+  return <CategoryList categories={categories} />;
+};
+
+export default Home;
