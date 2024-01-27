1082ebf11bb7e14ee33cabe6e29c22cf60f58f42
weeee9 <teletubby332@gmail.com>
2024-01-27 15:00:28 +0800

    refactor: refactor category data handling
    - Remove redundant code for category containers
    - Replace hardcoded category data with dynamically generated data from `categories` array
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,46 @@
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      subtitle: 'Shop Now',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      subtitle: 'Shop Now',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      subtitle: 'Shop Now',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      subtitle: 'Shop Now',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      subtitle: 'Shop Now',
+    },
+  ];
+
   return (
     <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      {categories.map((category) => {
+        const { id, title, subtitle } = category;
+        return (
+          <div key={id} className='category-container'>
+            {/* <img /> */}
+            <div className='category-body-container'>
+              <h2>{title}</h2>
+              <p>{subtitle}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
