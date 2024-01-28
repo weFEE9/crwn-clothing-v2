7419c4a5324c33bebd314a18e50cf16890ef8b50
weeee9 <teletubby332@gmail.com>
2024-01-28 11:06:21 +0800

    feat: create a basic shop route

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,19 @@ import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 
 const App = () => {
+  const Shop = () => {
+    return (
+      <div>
+        <h1>I am the shop</h1>
+      </div>
+    );
+  };
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
   );
