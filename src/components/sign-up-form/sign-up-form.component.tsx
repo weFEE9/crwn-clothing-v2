51d30182d3cb4560c1569e02f0ec688d71b7546d
weeee9 <teletubby332@gmail.com>
2024-01-28 11:20:59 +0800

    feat: refactor SignUpForm component to use useState hook and update form fields and handlers
    - Modify the `SignUpForm` component to use the `useState` hook
    - Add form fields for `displayName`, `email`, `password`, and `confirmPassword`
    - Implement the `handleFormChange` function to update the form fields
    - Update the JSX code to use the new form fields and the `handleFormChange` function
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- src/components/sign-up-form/sign-up-form.component.tsx
+++ src/components/sign-up-form/sign-up-form.component.tsx
@@ -1,21 +1,63 @@
+import { useState } from 'react';
+
+const defautlFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
 const SignUpForm = () => {
   const onSingUpFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {};
+
+  const [formFields, setFormFields] = useState(defautlFormFields);
+
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
 
   return (
     <div>
       <h1>Sign up with your email and password</h1>
       <form onSubmit={onSingUpFormSubmit}>
-        <label htmlFor=''>Display Name</label>
-        <input type='text' required />
+        <label>Display Name</label>
+        <input
+          type='text'
+          required
+          name='displayName'
+          value={displayName}
+          onChange={handleFormChange}
+        />
 
-        <label htmlFor=''>Email</label>
-        <input type='email' required />
+        <label>Email</label>
+        <input
+          type='email'
+          required
+          name='email'
+          value={email}
+          onChange={handleFormChange}
+        />
 
-        <label htmlFor=''>Password</label>
-        <input type='password' required />
+        <label>Password</label>
+        <input
+          type='password'
+          required
+          name='password'
+          value={password}
+          onChange={handleFormChange}
+        />
 
-        <label htmlFor=''>Confirm Password</label>
-        <input type='password' required />
+        <label>Confirm Password</label>
+        <input
+          type='password'
+          required
+          name='confirmPassword'
+          value={confirmPassword}
+          onChange={handleFormChange}
+        />
 
         <button type='submit'>Sign Up</button>
       </form>
