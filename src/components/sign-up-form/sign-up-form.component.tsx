2f9fbdc701b5f8a10ff09939d5886cf8daca122c
weeee9 <teletubby332@gmail.com>
2024-01-28 11:42:00 +0800

    feat: refactor user creation process and form handling
    - Change the function name from `onSingUpFormSubmit` to `handleFormSubmit`
    - Add form validation to check if password and confirmPassword match
    - Implement `createAuthUserWithEmailAndPassword` function
    - Implement `createUserDocumentFromAuth` function
    - Handle user creation error (commented out code)
    - Add `resetFormFields` function to reset form fields
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- src/components/sign-up-form/sign-up-form.component.tsx
+++ src/components/sign-up-form/sign-up-form.component.tsx
@@ -8,7 +8,48 @@ const defautlFormFields = {
 };
 
 const SignUpForm = () => {
-  const onSingUpFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {};
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    // TODO: implement createAuthUserWithEmailAndPassword method with google firebase
+    const createAuthUserWithEmailAndPassword = async (
+      email: String,
+      passwrod: String
+    ) => {};
+
+    // TODO: implement createUserDocumentFromAuth method with google firebase
+    const createUserDocumentFromAuth = async (
+      authUser: any,
+      additionalData: any
+    ) => {};
+
+    try {
+      const user = await createAuthUserWithEmailAndPassword(email, password);
+      console.log(user);
+
+      await createUserDocumentFromAuth(user, { displayName });
+
+      resetFormFields();
+    } catch (error: unknown) {
+      // TODO: implement error handling
+      //
+      // const { code } = error;
+      // if (code === 'auth/email-already-in-use') {
+      //   alert('Email already in use');
+      // } else {
+      //   console.log('user creation encountered an error', error);
+      // }
+    }
+  };
+
+  const resetFormFields = () => {
+    setFormFields(defautlFormFields);
+  };
 
   const [formFields, setFormFields] = useState(defautlFormFields);
 
@@ -22,7 +63,7 @@ const SignUpForm = () => {
   return (
     <div>
       <h1>Sign up with your email and password</h1>
-      <form onSubmit={onSingUpFormSubmit}>
+      <form onSubmit={handleFormSubmit}>
         <label>Display Name</label>
         <input
           type='text'
