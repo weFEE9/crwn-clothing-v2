0d4989b5f7ef4818b3b2f9481a8b23a44561423d
weeee9 <teletubby332@gmail.com>
2024-02-01 21:06:14 +0800

    feat: add Firebase authentication and Firestore functions
    - Add `dotenv` and `firebase` dependencies to `package.json`
    - Create a new file `src/utils/firebase/firebase.utils.ts` with Firebase authentication and Firestore functions
    - Initialize Firebase with the provided configuration
    - Export authentication, sign-in, and Firestore functions
    - Define `UserAuth` type and `createUserDocumentFromAuth` function for creating user documents in Firestore
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

---
+++ src/utils/firebase/firebase.utils.ts
@@ -0,0 +1,68 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+import { FirebaseError } from '@firebase/util';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const auth = getAuth(firebaseApp);
+
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export type UserAuth = {
+  uid: string;
+  email: string | null;
+  displayName: string | null;
+};
+
+export const createUserDocumentFromAuth = async (userAuth: UserAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  let userSnapshot;
+  try {
+    userSnapshot = await getDoc(userDocRef);
+  } catch (error) {
+    if (error instanceof FirebaseError) {
+      console.log('Error getting user', error.code, error.message);
+    }
+  }
+
+  if (userSnapshot && !userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createAt,
+      });
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        console.log('Error creating user', error.code, error.message);
+      }
+    }
+  }
+
+  return userDocRef;
+};
